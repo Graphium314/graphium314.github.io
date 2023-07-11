074834a5d569423d818fdd83174ae6c922c53d01
Graphium314 <graphium142857@gmail.com>
2023-07-12 00:01:13 +0900

    feat: mdxに対応

---
+++ src/components/title.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+const siteTitle = "Graphiumのブログ";
+
+const Title: React.FC<{ children: string } | { title: string }> = (props) => {
+  const title = "children" in props ? props.children : props.title;
+  const realTitle = title === "" ? siteTitle : `${title} - ${siteTitle}`;
+  return (
+    <Head>
+      <title>{realTitle}</title>
+    </Head>
+  );
+};
+export default Title;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,32 @@
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import Link from "next/link";
+
+import "../styles/globals.scss";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <div id="layout_wrapper">
+        <Head>
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+        </Head>
+        <header>
+          <a href="/" className="navbar-brand">
+            Graphiumのブログ
+          </a>
+        </header>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </div>
+      <footer>
+        <div>
+          <p>
+            Graphium314
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
