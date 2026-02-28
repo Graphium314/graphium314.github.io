92384830fec64f0338a76e96da19908b866cc4a2
Graphium314 <graphium142857@gmail.com>
2026-02-28 17:08:39 +0900

    404のページを作成

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <article className="not-found">
+      <h1>404 Not Found</h1>
+      <p>お探しのページは削除されたか、URLが間違っている可能性があります。</p>
+    </article>
+  );
+}
